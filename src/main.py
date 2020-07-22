ef44c6d2afca40e04409a352d0d8da21be26f528
zish <37426682+Zi-SH@users.noreply.github.com>
2020-07-21 20:43:55 -0400

    Updated imports to include new filter class Updated main to utilize new config.py variables Added variable to track loaded modules Added on_message, on_command_error, on_ready and bot connection logic

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import json, sys, os, logging
 from discord.ext import commands
-from AmadeusKurisu import config
+from AmadeusKurisu import config, filter
 from AmadeusKurisu.discord.modules import utils
 
 # Amadeus System 0.01
@@ -19,12 +19,15 @@ from AmadeusKurisu.discord.modules import utils
 
 # Initialize bot client
 client = commands.Bot(
-    command_prefix=config.bot["prefix"],
-    description=config.bot["desc"],
+    command_prefix=config.PREFIX,
+    description=config.DESCRIPTION,
     case_insensitive=True)
 
 ## Clear help command
 client.remove_command("help")
+
+## Constant variables
+loadedModules = []
 
 ## Command Functions
 @client.command(pass_context=True)
@@ -43,7 +46,42 @@ async def reload(context, module):
 @client.command(pass_context=True)
 async def listmods(context):
     # TODO: Fancy embed
-    utils.send_message(context, utils.get_modules())
+    await utils.send_message(context, utils.get_modules())
+
+## Message functions
+@client.event
+async def on_message(message):
+    if not message.author.bot:
+        await client.process_commands(message)
+
+@client.event
+async def on_command_error(context,error):
+    if not isinstance(error, commands.errors.MissingRequiredArgument):
+        logging.error(error)
+
+@client.event
+async def on_ready():
+    logging.info(f"{client.user.name} has connected successfully")
+    logging.info("------")
+
+    modules = utils.get_modules()
+    modulePath = utils.get_module_path()
+
+    for module in modules:
+        if await utils.load_module(client, module):
+            loadedModules.append(module)
+    try:
+        await utils.load_module(client, modulePath + ".help")
+        loadedModules.append(modulePath + ".help")
+    except (AttributeError, ImportError) as e:
+        logging.critical("Help module failed to load.")
+        sys.exit("Fatal Error: Unable to load help module.")
+
+    logging.info("------")
+
+client.run(config.TOKEN)
+
+
 
 
 
