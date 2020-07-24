e8458ad4685d7e959338860507b09cbfeb627fc9
zish <37426682+Zi-SH@users.noreply.github.com>
2020-07-24 01:41:36 -0400

    Removed unneeded code due to how modules are handled.

--- src/AmadeusKurisu/discord/modules/debug.py
+++ src/AmadeusKurisu/discord/modules/debug.py
@@ -3,8 +3,6 @@ from discord.ext import commands
 class debug(commands.Cog):
     def __init__(self, client):
         self.client = client
-        global modName
-        modName = self.__class__.__name__
 
     @commands.command(pass_context=True)
     async def nullpo(self, ctx):
